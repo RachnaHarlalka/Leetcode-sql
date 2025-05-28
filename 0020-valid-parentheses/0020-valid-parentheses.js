4034590d0059e69e4d10e9205ed16472827a155a
RachnaHarlalka <71887838+RachnaHarlalka@users.noreply.github.com>
2025-05-28 15:36:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let obj={
+        ")":"(",
+        "}":"{",
+        "]":"["
+    }
+    let flag=true;
+    let stack=[]
+    if(s.length===1) return false
+    s.split("").forEach((item)=>{
+        if(item==="(" || item==="{" || item==="["){
+            stack.push(item)
+        }
+        else {
+            if(stack.length===0) {
+                flag=false;
+                return false;
+            }
+            const top=stack[stack.length-1];
+            if(top!==obj[item]){
+                flag=false;
+                return false;
+            }
+            else stack.pop()
+        }
+    })
+    if(flag) return true
+    else return false
+};
