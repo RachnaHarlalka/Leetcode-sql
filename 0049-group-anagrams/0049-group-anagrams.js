28e034780e38161d9d7ebc971531809a87032df2
RachnaHarlalka <71887838+RachnaHarlalka@users.noreply.github.com>
2026-02-16 15:58:43 +0530

    Time: 194 ms (5.05%), Space: 72.1 MB (5.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,12 +3,15 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let obj={};
-    for(let item of strs){
-        const val=item.split("").sort().join("")
-        if(obj[val]) obj[val].push(item);
-        else obj[val]=[item]
+    let obj = {};
+    for(let i=0;i<strs.length;i++){
+        const key = strs[i].split("").sort().join("");
+        console.log({key},strs[i])
+        if(obj[key] != undefined){
+            obj[key].push(strs[i])
+        }
+        else obj[key] = [strs[i]]
     }
-
+    console.log({obj})
     return Object.values(obj)
 };
