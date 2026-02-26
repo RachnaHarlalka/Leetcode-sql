8b13783b680ef21fb391f3cddc25526e685e9ee5
RachnaHarlalka <71887838+RachnaHarlalka@users.noreply.github.com>
2026-02-26 11:44:28 +0530

    Time: 0 ms (100%), Space: 64 MB (49.48%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+   let currSum = 0;
+   let maxSum = nums[0];
+   for(let i=0;i<nums.length;i++){
+    currSum +=nums[i];
+    maxSum = Math.max(currSum,maxSum);
+    if(currSum < 0)
+        currSum = 0
+   }
+   return maxSum
+};
