095929eadae349b20f0fca6bd8009e3d83ff4460
RachnaHarlalka <71887838+RachnaHarlalka@users.noreply.github.com>
2026-03-17 22:34:48 +0530

    Time: 0 ms (100%), Space: 53.3 MB (89.33%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -5,12 +5,39 @@
  */
 
 
-var searchMatrix = function(matrix, target) {
-    for(let i=0;i<matrix.length;i++){
-        for(let j=0;j<matrix[0].length;j++){
-            if(matrix[i][j]===target)
-                return true
+function findInsideRow(matrix,target,row){
+    let start = 0
+    let end = matrix[0].length-1;
+    while(start<=end){
+        let mid = Math.floor((start+end)/2)
+        if(target===matrix[row][mid]){
+            return true
+        }
+        else if(target<matrix[row][mid]){
+            end=mid-1
+        }
+        else {
+            start = mid+1
         }
     }
-    return false;
+    return false
+}
+var searchMatrix = function(matrix, target) {
+    const m = matrix.length; //3
+    const n = matrix[0].length; //4
+    let start = 0; //0
+    let end = m-1; //2 //0
+    while(start<=end){ 
+        let mid = Math.floor((start+end)/2); //1
+        if(target>=matrix[mid][0] && target<=matrix[mid][n-1]){
+            return findInsideRow(matrix,target,mid)
+        }
+        else if(target<matrix[mid][0]){
+            end=mid-1
+        }
+        else if(target>matrix[mid][n-1]){
+            start=mid+1
+        }
+    }
+    return false
 };
